5c8cfce8fa1445f55f5231d4410e2b5a0aaea9cf
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2024-10-30 17:09:19 +0400

    homework

---
+++ script.js
@@ -0,0 +1,40 @@
+let Name1="Taia";
+let Age1=21;
+let Surname1 = "Gvianishvili";
+let Phone1=577616255;
+let Email1="gvianishvilitaia@gamil.com"
+let Isadmin1="true"
+let Isnotadmin1="false"
+console.log("My name is",Name1,",My age is",Age1,",My surname is",Surname1,",My email is",Email1,"My phone number is:",Phone1,"Am i 18+ years old? --",Isadmin1 );
+let Name2="Toma";
+let Age2=17;
+let Surname2 = "Tsertsvadze";
+let Phone2=577616464;
+let Email2="tsertsvadzetoma@gamil.com"
+let Isadmin2="true"
+let Isnotadmin2="false"
+console.log("My name is",Name2,",My age is",Age2,",My surname is",Surname2,",My email is",Email2,"My phone number is:",Phone2,"Am i 18+ years old? - ",Isnotadmin2);
+let Name3="Petre";
+let Age3=20;
+let Surname3 = "Bagashvili";
+let Phone3=599437300;
+let Email3="bagashvilipetre@gamil.com"
+let Isadmin3="true"
+let Isnotadmin3="false"
+console.log("My name is",Name3,",My age is",Age3,",My surname is",Surname3,",My email is",Email3,"My phone number is:",Phone3,"Am i 18+ years old? - ",Isadmin3);
+let Name4="Tamar";
+let Age4=15;
+let Surname4 = "Bagashvili";
+let Phone4=599335183;
+let Email4="bagashvilitamar@gamil.com"
+let Isadmin4="true"
+let Isnotadmin4="false"
+console.log("My name is",Name4,",My age is",Age4,",My surname is",Surname4,",My email is",Email4,"My phone number is:",Phone4,"Am i 18+ years old? - ",Isnotadmin4);
+let Name="Tinatin";
+let Age=20;
+let Surname = "Bagashvili";
+let Phone=599357300;
+let Email="bagashvilitinatin@gamil.com"
+let Isadmin="true"
+let Isnotadmin="false"
+console.log("My name is",Name,",My age is",Age,",My surname is",Surname,",My email is",Email,"My phone number is:",Phone,"Am i 18+ years old? - ",Isadmin);
